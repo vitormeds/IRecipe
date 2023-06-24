127c8bef0711daada4e018abd7b280517143cbad
Vitor Mendes <vitor.mendes.souza@outlook.com>
2023-06-24 18:08:19 -0300

    Initial Commit

---
+++ IRecipe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IRecipe
+//
+//  Created by Vitor Mendes on 24/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
